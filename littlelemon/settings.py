812fadd6ead258d4ef7543b6dc441274e0377f74
nngobese023 <nngobese023@student.wethinkcode.co.za>
2025-05-23 09:57:25 +0200

    last commit

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -84,8 +84,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'LittleLemon',
-        'USER': 'admindjango',
-        'PASSWORD': 'employee@123!',
+        'USER': 'root',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {
